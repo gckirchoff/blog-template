be7e4f1ad4819ec4247356801eeb55641f5de85e
squinja <eschaale@gmail.com>
2024-11-21 15:27:10 -0500

    Create front end pg for making gallery

--- src/routes/(site)/dev/admin/+page.svelte
+++ src/routes/(site)/dev/admin/+page.svelte
@@ -1,3 +1,13 @@
 <h1>Admin panel</h1>
 
-<a href="/dev/admin/post">Create Post</a>
+<div class="admin-tasks">
+	<a href="/dev/admin/post">Create Post</a>
+	<a href="/dev/admin/gallery">Create Gallery</a>
+</div>
+
+<style lang="scss">
+    .admin-tasks {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
