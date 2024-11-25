63bb4432b49e48e53532ea3f0c4258d41244ab76
squinja <eschaale@gmail.com>
2024-11-24 19:38:34 -0500

    Create gallery req post type

---
+++ src/routes/api/v1/gallery/constants.ts
@@ -0,0 +1,10 @@
+export type GalleryReqPostBody = {
+	title: string;
+	description: string;
+	// categories: string[];
+	// authors: string[];
+	// content: string;
+	// usables: { [id: string]: Usable };
+	// published: boolean;
+	coverImage?: string;
+};
